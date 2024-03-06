850fc6e66f67a50fe08cd23c0dc4b066fa093d3e
apsinghdev <ajeetpratap517@gmail.com>
2024-03-06 13:25:20 +0530

    add canvas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-
-
 function App() {
-  
-
-  return (<div></div>)
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "700px",
+        width: "100%",
+      }}
+    >
+      <canvas
+        style={{
+          height: "70vh",
+          width: "70vh",
+          border: "0.5px solid black",
+          boxShadow: "0 0 5px #000",
+        }}
+      ></canvas>
+    </div>
+  );
 }
 
-export default App
+export default App;
