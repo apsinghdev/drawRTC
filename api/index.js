99a1a71df53c0ea140ec5889872ee9afe27fe4cc
ajeet <ajeetpratap517@gmail.com>
2024-03-08 23:54:41 +0530

    implement socket to server side

--- api/index.js
+++ api/index.js
@@ -1,1 +1,21 @@
 // server file
+import express from "express";
+import {Server} from "socket.io";
+import {createServer} from "http";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const PORT = 8080;
+
+app.get('/', (req, res)=>{
+    console.log('running')
+})
+
+io.on('connection', (socket)=>{
+    console.log('user connected socket')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server running on ${PORT}`)
+})
