ea79f9c8f54a8f243ff3a281db5e3175f379f7ee
apsinghdev <ajeetpratap517@gmail.com>
2024-05-13 13:48:23 +0530

    setup context for state management

---
+++ client/src/components/MenuItem.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+function MenuItem(props){
+    return(
+        <div>
+            <h1>{props.feat}</h1>
+        </div>
+    )
+}
+
+export default MenuItem;
