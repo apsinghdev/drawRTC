86d51378955cabdfe7ab17cecb8dd75f628e2bde
apsinghdev <ajeetpratap517@gmail.com>
2024-05-22 14:34:58 +0530

    add recoil

--- client/src/components/Eraser.jsx
+++ client/src/components/Eraser.jsx
@@ -1,12 +1,17 @@
 import eraserImg from "../assets/eraser.png";
 
 function Eraser(){
+
+  function clickHandler(){
+
+  }
+
     return (
       <div className="flex p-1 items-center justify-around mt-3">
         <h1 className="font-sans text-lg antialiased font-semibold text-white">
           Eraser :
         </h1>
-        <img src={eraserImg} className="h-8 w-8 rounded cursor-pointer"></img>
+        <img src={eraserImg} className="h-8 w-8 rounded cursor-pointer" onClick={clickHandler} ></img>
       </div>
     );
 }
