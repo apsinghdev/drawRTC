14b794bb35031a596695e8cfd8a6fe44a55e8fd5
apsinghdev <ajeetpratap517@gmail.com>
2024-05-27 15:40:51 +0530

    add canvas state

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -3,11 +3,16 @@ import Socials from "./Socials";
 import SponsorBtn from "./SponsorBtn";
 
 function Menu(){
+
+  function clickHandlerPdf(){
+    
+  }
+
     return (
       <div className="w-52 h-71 rounded-xl bg-gradient-to-r from-slate-900 to-slate-700 absolute left-52 top-8 rounded-lg shadow-xl">
         <MenuItem feat="Start Collaboration"></MenuItem>
         <MenuItem feat="Start Chat"></MenuItem>
-        <MenuItem feat="Save as pdf"></MenuItem>
+        <MenuItem feat="Save as pdf" onClick={clickHandlerPdf}></MenuItem>
         <MenuItem feat="Save as png"></MenuItem>
         <SponsorBtn></SponsorBtn>
         <Socials></Socials>
