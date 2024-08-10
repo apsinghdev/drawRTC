c3c08b4110ea803fd05f1f69889a7465a7c25c44
apsinghdev <ajeetpratap517@gmail.com>
2024-08-10 09:19:12 +0530

    Change url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
-const PORT = "http://localhost:8000";
+const PORT =
+  "https://draw-rtc-96kr12q0u-ajeet-pratap-singhs-projects.vercel.app/";
 
 const socket = io(PORT);
 
 socket.on("connect", () => {
-    console.log("connected");
+  console.log("connected");
 });
 
 export default socket;
